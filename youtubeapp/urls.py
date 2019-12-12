195b304711ba8dd08faa8c1e19a30a12f3c2509a
Garry Ariel <garry@alterra.id>
2019-12-13 01:21:22 +0700

    Tambah Video

--- youtubeapp/urls.py
+++ youtubeapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('video/<int:video_id>', views.show, name="show"),
     path('search/', views.search, name="search"),
     path('upload/', views.upload, name="upload"),
-    path('upload_selesai/', views.upload_selesai, name="upload_selesai")
+    path('upload_selesai/', views.upload_selesai, name="upload_selesai"),
+    path('trending/', views.trending, name="trending")
 ]

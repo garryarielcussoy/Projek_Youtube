7a5c7171ca93af696f6b9edf3b65081df5266fb2
Garry Ariel <garry@alterra.id>
2019-12-12 14:41:46 +0700

    Setting Model

--- youtubeapp/models.py
+++ youtubeapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    judul = models.CharField(max_length=200, default='')
+    url_video = models.CharField(max_length=200, default='')
+    author = models.CharField(max_length=200, default='')
+    tanggal_upload = models.DateTimeField('Waktu Upload')
+
+    def __str__(self):
+        return self.judul

962699b8a8c1571342c1bee2990de1688406f2a6
Garry Ariel <garry@alterra.id>
2019-12-12 18:00:24 +0700

    Ubah Model

--- youtubeapp/models.py
+++ youtubeapp/models.py
@@ -8,13 +8,7 @@ class Video(models.Model):
     waktu_upload = models.DateTimeField('Waktu Upload')
     foto_author = models.CharField(max_length=200, default='')
     deskripsi = models.TextField(default='')
+    unik = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.judul
-
-class URLUnik(models.Model):
-    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
-    unik = models.CharField(max_length=255, default='')
-
-    def __str__(self):
-        return self.video_id
--- youtubeapp/views.py
+++ youtubeapp/views.py
@@ -12,9 +12,7 @@ def index(request):
 
 def show(request, video_id):
     video = Video.objects.get(pk = video_id)
-    unique_key = video.url_video[32:]
     kirim = {
         'video' : video,
-        'unik' : unique_key
     }
     return render(request, 'youtubeapp/show.html', kirim)

8e3576e3c2b85bf9930a8fcf305eff38f339e951
Garry Ariel <garry@alterra.id>
2019-12-12 15:25:01 +0700

    Rename Salah Satu Field di Model

--- youtubeapp/models.py
+++ youtubeapp/models.py
@@ -5,7 +5,7 @@ class Video(models.Model):
     judul = models.CharField(max_length=200, default='')
     url_video = models.CharField(max_length=200, default='')
     author = models.CharField(max_length=200, default='')
-    tanggal_upload = models.DateTimeField('Waktu Upload')
+    waktu_upload = models.DateTimeField('Waktu Upload')
     foto_author = models.CharField(max_length=200, default='')
 
     def __str__(self):
--- youtubeapp/views.py
+++ youtubeapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Video
 
 # Create your views here.
 def index(request):
-    return render(request, 'youtubeapp/index.html', {})
+    kumpulan_video = Video.objects.all()
+    kirim = {
+        'kumpulan_video' : kumpulan_video
+    }
+    return render(request, 'youtubeapp/index.html', kirim)
